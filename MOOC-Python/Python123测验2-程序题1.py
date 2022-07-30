7d00934925a3a6b1700ec9eed6540cae1b846684
Albert-lau-DS <805499918@qq.com>
2022-07-31 02:32:06 +0800

    Initial commit

---
+++ MOOC-Python/Python123测验2-程序题1.py
@@ -0,0 +1,12 @@
+# turtle八边形绘制.py
+# 使用turtle库，绘制一个八边形。
+
+# 请在横线中填写Python表达式或语句，实现所需要的功能
+# 注意：补充代码将以匹配方式评阅，代码中不要出现空格
+
+import turtle as t
+
+t.pensize(2)
+for i in range(1, 9):# for i in range(8)
+    t.fd(100)
+    t.left(45)
